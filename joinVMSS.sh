b1821c978781aaddad6a8a053bf3a48ee967f528
danlai-ms <47874156+danlai-ms@users.noreply.github.com>
2025-04-15 22:09:55 +0000

    u

--- joinVMSS.sh
+++ joinVMSS.sh
@@ -295,6 +295,42 @@ fi
 # DNC_URL="http://localhost:$LOCAL_PORT"
 # echo "Successfully port forwarded to DNC: $DNC_URL"
 
+LABEL_SELECTOR="app=dnc"  # Replace with the label selector for the DNC pod
+LOCAL_PORT=9000  # Local port to forward
+REMOTE_PORT=9000  # Pod's port to forward
+DNC_POD="dnc-7b76546bfd-kcc4d"
+
+# Start port forwarding
+echo "Starting port forwarding from localhost:$LOCAL_PORT to $DNC_POD:$REMOTE_PORT..."
+kubectl port-forward -n "$NAMESPACE" pod/"$DNC_POD" "$LOCAL_PORT:$REMOTE_PORT" & PORT_FORWARD_PID=$!
+
+# Wait for port forwarding to establish
+sleep 20
+
+# Check if the port forwarding process is running
+if ! kill -0 $PORT_FORWARD_PID 2>/dev/null; then
+  echo "Error: Port forwarding failed to start"
+  exit 1
+fi
+
+# Log the forwarded URL
+DNC_URL="http://localhost:$LOCAL_PORT"
+echo "Successfully port forwarded to DNC: $DNC_URL"
+
+
+# Stop port forwarding
+echo "Stopping port forwarding..."
+kill $PORT_FORWARD_PID
+
+sleep 20
+
+# Verify the process has stopped
+if kill -0 $PORT_FORWARD_PID 2>/dev/null; then
+  echo "Error: Failed to stop port forwarding"
+  exit 1
+fi
+
+echo "Port forwarding stopped successfully."
 
 
 ############################ Register node with DNC  ############################
