e50068145140657da352401cb1c18c2a62fff2e1
danlai-ms <47874156+danlai-ms@users.noreply.github.com>
2025-04-18 11:46:33 +0000

    u

--- joinVMSS.sh
+++ joinVMSS.sh
@@ -203,13 +203,13 @@ fi
 # echo "Deploying dnc_configmap.yaml to namespace default..."
 # kubectl apply -f dnc_configmap.yaml -n default
 
-echo "Deploying dnc_deployment.yaml to namespace default..."
-# TODO: deploy DNC needs to assign MI that can access DB to the dnc node
-kubectl apply -f dnc_deployment.yaml -n default
+# echo "Deploying dnc_deployment.yaml to namespace default..."
+# # TODO: deploy DNC needs to assign MI that can access DB to the dnc node
+# kubectl apply -f dnc_deployment.yaml -n default
 
-# Label the nodes to specify the cx
-kubectl label node linuxpool12000000 cx=vm1
-kubectl label node linuxpool121000000 cx=vm2
+# # Label the nodes to specify the cx
+# kubectl label node linuxpool12000000 cx=vm1
+# kubectl label node linuxpool121000000 cx=vm2
 
 # echo "Deploying container1.yaml to node cx=vm1..."
 # kubectl apply -f container1.yaml -n default
@@ -219,30 +219,30 @@ kubectl label node linuxpool121000000 cx=vm2
 
 #########################################################################
 ########################### Label data plane nodes ###########################
-# Variables
-NODE_LABEL_KEY="dncnode"  # Key for the label
+# # Variables
+# NODE_LABEL_KEY="dncnode"  # Key for the label
 
-# Function to label a node
-label_node() {
-  local node_name=$1
-  local label_key=$2
-  local label_value=$3
+# # Function to label a node
+# label_node() {
+#   local node_name=$1
+#   local label_key=$2
+#   local label_value=$3
 
-  echo "Labeling node: $node_name with label: $label_key=$label_value"
-  kubectl label node "$node_name" "$label_key=$label_value" --overwrite
-  echo "Successfully labeled node: $node_name with label: $label_key=$label_value"
-}
+#   echo "Labeling node: $node_name with label: $label_key=$label_value"
+#   kubectl label node "$node_name" "$label_key=$label_value" --overwrite
+#   echo "Successfully labeled node: $node_name with label: $label_key=$label_value"
+# }
 
-# Get the list of nodes to label
-# Replace this with your logic to fetch node names dynamically
-NODE_NAMES=("linuxpool12000000" "linuxpool121000000")
+# # Get the list of nodes to label
+# # Replace this with your logic to fetch node names dynamically
+# NODE_NAMES=("linuxpool12000000" "linuxpool121000000")
 
-# Label each node
-for NODE_NAME in $NODE_NAMES; do
-  label_node "$NODE_NAME" "$NODE_LABEL_KEY" "$NODE_NAME"
-done
+# # Label each node
+# for NODE_NAME in $NODE_NAMES; do
+#   label_node "$NODE_NAME" "$NODE_LABEL_KEY" "$NODE_NAME"
+# done
 
-echo "All nodes have been successfully labeled."
+# echo "All nodes have been successfully labeled."
 
 ######################## Port Forwarding to DNC ########################
 # NAMESPACE="default"
