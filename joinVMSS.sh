b56cc19cc224553b82a374b05c50c8a301f5579a
danlai-ms <47874156+danlai-ms@users.noreply.github.com>
2025-04-24 11:18:48 +0000

    u

--- joinVMSS.sh
+++ joinVMSS.sh
@@ -414,7 +414,7 @@ fi
 
 
 # Variables
-END_TIME=$((SECONDS + 300))  # 30 minutes = 1800 seconds
+END_TIME=$((SECONDS + 180))  # 30 minutes = 1800 seconds
 INTERVAL=10  # Interval between iterations in seconds
 
 echo "Starting the loop for 30 minutes..."
